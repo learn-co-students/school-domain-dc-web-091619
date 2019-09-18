b5b34af9f8ac80e2681cca0eb9d5cbd4cefc3168
chungcha <charlieschung@gmail.com>
2019-09-18 11:29:41 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require "pry"
+
+class School
+    def initialize (school)
+        @school = school
+        @roster = {}
+    end
+    
+    def roster
+        @roster
+    end
+
+    def add_student (name,grade)
+        @roster[grade]||= @roster[grade]=[]
+        @roster[grade]<<name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end    
+
+    def sort
+        roster.each do |grade, array|
+            roster[grade]=array.sort()
+        end
+        roster
+    end
+end
